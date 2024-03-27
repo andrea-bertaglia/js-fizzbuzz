d1cd7936af0cd4804455201f127d1ce2e6cea79d
andrea-bertaglia <andreabertaglia@gmail.com>
2024-03-27 17:11:18 +0100

    feat: script js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 // Test
 console.log("Hello World!");
+
+// 1. Inizializzo la variabile di appoggio
+let result = "contenuto di result"; // string
+console.log(result);
+
+// 2. Creo un ciclo da 1 a 100
+for (let i = 1; i <=100; i++) {
+
+    // 3. Assegnazione valori ai multipli di 3 e 5
+    if (i % 15 === 0) {
+        result = "FizzBuzz"
+    } else if (i % 3 === 0 ) {
+        result = "Fizz";
+    } else if (i % 5 === 0) {
+        result = "Buzz";
+    } else {
+        result = i;
+    }
+
+
+    console.log(result);
+}
